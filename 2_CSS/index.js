b52ac8d48719950e876e0975abaedecec18b5843
Pradosh <mfsi.pradoshs@gmail.com>
2023-07-06 16:02:55 +0530

    Second commit

--- 2_CSS/index.js
+++ 2_CSS/index.js
@@ -1,15 +1,38 @@
-const arr2 = [
-  ["State11", "State12", "State13"],
-  ["State21", "State22", "State23"],
-  ["State31", "State32", "State33"],
-];
-const arr = ["Country1", "Country2", "Country3"];
-let country = document.getElementById("countryList");
-for (let i = 0; i < arr.length; i++) {
-  var option = document.createElement("option");
-  option.value = arr[i];
-  country.appendChild(option);
-}
+fetch("https://www.universal-tutorial.com/api/countries/", {
+  method: "GET",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJtZnNpLnByYWRvc2hzQGdtYWlsLmNvbSIsImFwaV90b2tlbiI6Ik1ZUEFPS2J2b0hfUV9CaVBIWU5vMlc4VlBRRVAtTTNfN0FLREgyMlpjUkVyRVpUb0M0WkdyU0c1QUhxX0w0bl9XR0kifSwiZXhwIjoxNjg4NzE2NTI3fQ.Np0okMWDxN_87UPbMEgIS3Vo0XpIDH9gAWh0R4KFYTE",
+    Accept: "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    let arr = data;
+
+    let country = document.getElementById("country");
+    let country2 = document.getElementById("country2");
+    for (let i = 0; i < arr.length; i++) {
+      let option = document.createElement("option");
+      let option2 = document.createElement("option");
+      option.value = arr[i].country_name;
+      option.innerText = arr[i].country_name;
+      option2.value = arr[i].country_name;
+      option2.innerText = arr[i].country_name;
+      country.appendChild(option);
+      country2.appendChild(option2);
+    }
+    // let country = document.getElementById("countryList");
+    // for (let i = 0; i < arr.length; i++) {
+    //   var option = document.createElement("option");
+    //   option.value = arr[i].country_name;
+    //   country.appendChild(option);
+    // }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 function dispName() {
   const file = document.getElementById("dp");
@@ -33,56 +56,94 @@ function dispName() {
   }
 }
 
-function popStates() {
+async function popStates() {
   let country = document.getElementById("country").value;
-  let state = document.getElementById("stateList");
-  state.innerHTML = "";
+  console.log(country);
+  document.getElementById("state").value = "";
   if (country != "") {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] == country) {
-        for (let j = 0; j < arr2[i].length; j++) {
+    let url = "https://www.universal-tutorial.com/api/states/" + country;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJtZnNpLnByYWRvc2hzQGdtYWlsLmNvbSIsImFwaV90b2tlbiI6Ik1ZUEFPS2J2b0hfUV9CaVBIWU5vMlc4VlBRRVAtTTNfN0FLREgyMlpjUkVyRVpUb0M0WkdyU0c1QUhxX0w0bl9XR0kifSwiZXhwIjoxNjg4NzE2NTI3fQ.Np0okMWDxN_87UPbMEgIS3Vo0XpIDH9gAWh0R4KFYTE",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let arr = data;
+        let state = document.getElementById("state");
+        for (let i = 0; i < arr.length; i++) {
           var option = document.createElement("option");
-          option.value = arr2[i][j];
+          option.value = arr[i].state_name;
+          option.innerText = arr[i].state_name;
           state.appendChild(option);
         }
-      }
-    }
+        // let state = document.getElementById("stateList");
+        // state.innerHTML = "";
+        // for (let i = 0; i < arr.length; i++) {
+        //   var option = document.createElement("option");
+        //   option.value = arr[i].state_name;
+        //   state.appendChild(option);
+        // }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
-function popStates2() {
+async function popStates2() {
   let country = document.getElementById("country2").value;
-  let state = document.getElementById("stateList2");
-  state.innerHTML = "";
+  document.getElementById("state2").value = "";
   if (country != "") {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] == country) {
-        for (let j = 0; j < arr2[i].length; j++) {
+    let url = "https://www.universal-tutorial.com/api/states/" + country;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJtZnNpLnByYWRvc2hzQGdtYWlsLmNvbSIsImFwaV90b2tlbiI6Ik1ZUEFPS2J2b0hfUV9CaVBIWU5vMlc4VlBRRVAtTTNfN0FLREgyMlpjUkVyRVpUb0M0WkdyU0c1QUhxX0w0bl9XR0kifSwiZXhwIjoxNjg4NzE2NTI3fQ.Np0okMWDxN_87UPbMEgIS3Vo0XpIDH9gAWh0R4KFYTE",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let arr = data;
+        let state = document.getElementById("state2");
+        for (let i = 0; i < arr.length; i++) {
           var option = document.createElement("option");
-          option.value = arr2[i][j];
+          option.value = arr[i].state_name;
+          option.innerText = arr[i].state_name;
           state.appendChild(option);
         }
-      }
-    }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
 
 function popHobby() {
   let hobby = document.getElementById("hobby").value;
-  if (hobby.trim()) {
-    let list = document.getElementById("hobbyList");
-    list.innerHTML = "";
-    let hobbyArr = ["Singing", "Dancing", "Playing"];
-    for (let i = 0; i < hobbyArr.length; i++) {
-      if (!hobby.includes(hobbyArr[i])) {
-        let option = document.createElement("option");
-        option.value = hobby + "," + hobbyArr[i];
-        list.appendChild(option);
+  let list = document.getElementById("hobbyList");
+  let hobbyArr = ["Singing", "Dancing", "Playing"];
+  list.innerHTML = "";
+  for (let i = 0; i < hobbyArr.length; i++) {
+    if (!hobby.includes(hobbyArr[i])) {
+      let option = document.createElement("option");
+      if (hobby.trim() != "") {
+        option.value = hobby + ", " + hobbyArr[i];
+      } else {
+        option.value = hobbyArr[i];
       }
+      list.appendChild(option);
     }
   }
 }
 
-function func() {
+async function func() {
   let address = document.getElementById("addr");
   let line1 = document.getElementById("al1p");
   let line2 = document.getElementById("al2p");
@@ -101,6 +162,7 @@ function func() {
     line1.value = pre_line1;
     line2.value = pre_line2;
     country.value = pre_country;
+    popStates2();
     state.value = pre_state;
     city.value = pre_city;
     pin.value = pre_pin;
@@ -123,7 +185,13 @@ function validate() {
   if (fname.trim() == "" || lname.trim() == "") {
     msg.innerText = "Enter a valid name";
   }
-  if (!patternName.test(fname)) {
+  if (!patternName.test(fname) || !patternName.test(lname)) {
     msg.innerText = "Enter a valid name";
+  }
+  if (hobby.trim() == "" || hobby.trim() == "") {
+    msg.innerText = "Enter a valid name";
+  }
+  if (!patternName.test(hobby) || !patternName.test(hobby)) {
+    msg.innerText = "Enter a valid hobby";
   }
 }
